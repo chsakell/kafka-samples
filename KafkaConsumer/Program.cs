713f4b651c7fd430c462740b79cb51dbb65976cf
chsakell <chsakell@gmail.com>
2024-07-13 15:39:06 +0300

    Add project files.

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+using static Confluent.Kafka.ConfigPropertyNames;
+
+const string TOPIC = "myorders";
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    ClientId = "dotnet.producer",
+    Acks = Acks.All,
+    GroupId = "1",
+    AutoOffsetReset = AutoOffsetReset.Earliest,
+    EnableAutoCommit = true,
+    //MessageTimeoutMs = 300000,
+    //BatchNumMessages = 10,
+    //LingerMs = 10,
+    //CompressionType = CompressionType.Gzip,
+};
+
+var consumer = new ConsumerBuilder<string, double>(config)
+    .SetValueDeserializer(Deserializers.Double)
+.Build();
+
+consumer.Subscribe(TOPIC);
+
+while(true)
+{
+    var result = consumer.Consume();
+
+    Console.WriteLine($"{result.Message.Key}-{result.Message.Value}");
+}
+
+consumer.Close();
+
+Console.ReadKey();
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using Confluent.Kafka;
+
+const string TOPIC = "myorders";
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092",
+    ClientId = "dotnet.producer",
+    Acks = Acks.All,
+    //MessageTimeoutMs = 300000,
+    //BatchNumMessages = 10,
+    //LingerMs = 10,
+    //CompressionType = CompressionType.Gzip,
+};
+
+var producer = new ProducerBuilder<string, double>(config)
+    .SetValueSerializer(Serializers.Double)
+    .SetKeySerializer(Serializers.Utf8)
+    .Build();
+
+for (var i = 0; i < 20; i++)
+{
+    Random autoRand = new Random();
+    var message = new Message<string, double>()
+    {
+        Key = $"key-{i}",
+        Value = autoRand.NextDouble(),
+    };
+
+    var result = await producer.ProduceAsync(TOPIC, message);
+}
+
+Console.ReadKey();
